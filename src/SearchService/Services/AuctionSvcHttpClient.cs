1049eee885173a5d2dd276495a30647e53492ea8
Imtiyaz Hoque Hazari <imtiyaz.hoque@brainstation-23.com>
2023-11-21 10:46:49 +0600

    end of section 3, synchronous conn stablished btwn scvs

---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services
+{
+    public class AuctionSvcHttpClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public AuctionSvcHttpClient(IConfiguration configuration, HttpClient httpClient)
+        {
+            _configuration = configuration;
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Item>> GetItemsForSearchDb()
+        {
+            var lastUpdated = await DB.Find<Item, string>()
+            .Sort(x => x.Descending(x => x.UpdatedAt))
+            .Project(x => x.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+
+            return await _httpClient.GetFromJsonAsync<List<Item>>(_configuration["AuctionServiceUrl"]
+            +"/api/auctions?date"+lastUpdated);
+        }
+
+    }
+}
