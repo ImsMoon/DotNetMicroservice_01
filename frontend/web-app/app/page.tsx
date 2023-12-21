a887a7789a1a75c08a3ba10c22bcf00ec3de97d8
Imtiyaz Hoque Hazari <imtiyaz.hoque@brainstation-23.com>
2023-12-21 16:05:17 +0600

    nextjs app added and end of section 8

---
+++ frontend/web-app/app/page.tsx
@@ -0,0 +1,13 @@
+import Listings from "./auction/Listings";
+
+
+
+export default function Home() {
+  return (
+   <div>
+    <h3 className='text-3xl font-semibold'>
+      <Listings />
+    </h3>
+   </div>
+  )
+}
